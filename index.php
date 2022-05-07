b70a02ee0281279bb680974439db55e0fbd083ce
Orlina M Brabar <90068655+OrLina1125@users.noreply.github.com>
2022-05-07 11:16:54 +0900

    add Foreach loop 20

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Loop 17</title>
+</head>
+<body>
+    <?php
+    //Loop
+    //While loop 
+    //DO while loop
+    //For loop
+    //Foreach loop (ini)
+    $array = array("Daniel", "Jane", "Jacob","John", "Mariane");
+    foreach ($array as $loopdata){
+        echo "My name is ".$loopdata. "<br>";
+    }
+    ?>
+</body>
+</html>
